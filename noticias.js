b3b9a0f7c033205b66471f35ceca31b474fe7b1f
FahRamatis <fah.ramatis@gmail.com>
2019-04-16 13:54:12 -0300

    servidor http com verificacao de rota

--- noticias.js
+++ noticias.js
@@ -1,1 +1,17 @@
-console.log('Criando um site de noticias com NodeJS')
+var http = require('http')
+
+var server = http.createServer(function(req,res){
+    var categoria = req.url
+    if(categoria === '/tecnologia'){
+        return res.end('<html><body>Noticias de Tecnologia</body></html>')
+    } else if (categoria === '/moda') {
+        return res.end('<html><body>Noticias de Moda</body></html>')
+    } else if (categoria === '/beleza') {
+        return res.end('<html><body>Noticias de Beleza</body></html>')
+    } else {
+        return res.end('<html><body>Portal de Noticias</body></html>')
+    }
+});
+let port = 3000
+server.listen(port, function(){console.log(`funcionando na porta ${port}`)})
+
